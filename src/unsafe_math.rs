c14e2cfefc5be164ff2da2ced49436eccfa8c405
Leyton <leytontaylor23@gmail.com>
2022-12-01 14:29:16 -0600

    getAmount0Delta, getAmount1Delta

--- src/unsafe_math.rs
+++ src/unsafe_math.rs
@@ -1,5 +1,7 @@
+use ethers::types::U256;
+
 pub fn div_rounding_up(a: U256, b: U256) -> U256 {
-    let (quotent, remainder) = a.div_rem(&b);
+    let (quotent, remainder) = a.div_mod(b);
     if remainder.is_zero() {
         quotent
     } else {
