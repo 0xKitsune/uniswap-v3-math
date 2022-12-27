4780f2f2c9d6d117072c662c45b550b7679c2b25
Leyton <leytontaylor23@gmail.com>
2022-12-27 15:06:19 -0600

    Overflow/Underflow in tick_bit_map.rs, changed little endian/big endian to reflect least and most significant bits

--- src/tick_bit_map.rs
+++ src/tick_bit_map.rs
@@ -24,10 +24,10 @@ pub fn next_initialized_tick_within_one_word(
         let initialized = !masked.is_zero();
 
         let next = if initialized {
-            let le_bytes = &mut [0u8; 32];
-            masked.to_little_endian(le_bytes);
-            let most_significant_bit = le_bytes[0];
-            compressed - ((bit_pos - most_significant_bit) as i32 & tick_spacing)
+            let be_bytes = &mut [0u8; 32];
+            masked.to_big_endian(be_bytes);
+            let most_significant_bit = be_bytes[0];
+            compressed - ((bit_pos.overflowing_sub(most_significant_bit).0) as i32 & tick_spacing)
         } else {
             compressed - (bit_pos as i32 * tick_spacing)
         };
@@ -42,9 +42,10 @@ pub fn next_initialized_tick_within_one_word(
 
         let next = if initialized {
             let le_bytes = &mut [0u8; 32];
-            masked.to_big_endian(le_bytes);
+            masked.to_little_endian(le_bytes);
             let least_significant_bit = le_bytes[0];
-            (compressed + 1 + (least_significant_bit - bit_pos) as i32) * tick_spacing
+            (compressed + 1 + (least_significant_bit.overflowing_sub(bit_pos).0) as i32)
+                * tick_spacing
         } else {
             (compressed + 1 + 0xFF - bit_pos as i32) * tick_spacing
         };
