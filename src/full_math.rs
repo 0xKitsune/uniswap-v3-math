9f2b2490f1a5b4b87537ab4feaae0d299899cc66
0xKitsune <77890308+0xKitsune@users.noreply.github.com>
2022-12-29 00:08:24 -0500

    Development (#17)
    * exposed min and max tick constants as public, updated version and cargo lock
    
    * bumped version to 0.2.4
    
    * bumping to v 0.2.5
    
    * added constants for MIN_SQRT_RATIO and MAX_SQRT_RATIO, bumped version
    
    * removed unneeded import
    
    * Overflow/Underflow in tick_bit_map.rs, changed little endian/big endian to reflect least and most significant bits
    
    * patching next_initialized_tick_within_one_word
    
    * bumping version to 0.2.8
    
    * updated mulmod to use ruint
    
    * patched overflow on left shift during next_initialized_tick_within_one_word
    
    * patched compute swap step, updated I256 -> U256 conversion
    
    * patched mulmod, bumping version
    
    * patching mulmod to allow overflowing mul
    
    * patching muldiv  to allow overflowing/underflowing
    
    * updated mulmod to use ruint
    
    * fixed clippy warnings and merged main
    
    * bumped to 0.2.14
    
    Co-authored-by: Leyton <leytontaylor23@gmail.com>

--- src/full_math.rs
+++ src/full_math.rs
@@ -1,10 +1,25 @@
 use ethers::types::U256;
+use ruint::Uint;
 use std::ops::{BitAnd, BitOrAssign};
 
 use crate::error::UniswapV3MathError;
 
 pub fn mul_mod(a: U256, b: U256, denominator: U256) -> U256 {
-    a.overflowing_mul(b).0 % denominator
+    let mut a_le_bytes = [0_u8; 32];
+    a.to_little_endian(&mut a_le_bytes);
+    let a: Uint<256, 4> = ruint::Uint::from_le_bytes(a_le_bytes);
+
+    let mut b_le_bytes = [0_u8; 32];
+    b.to_little_endian(&mut b_le_bytes);
+    let b: Uint<256, 4> = ruint::Uint::from_le_bytes(b_le_bytes);
+
+    let mut d_le_bytes = [0_u8; 32];
+    denominator.to_little_endian(&mut d_le_bytes);
+    let d: Uint<256, 4> = ruint::Uint::from_le_bytes(d_le_bytes);
+
+    let result: [u8; 32] = a.mul_mod(b, d).to_le_bytes();
+
+    U256::from_little_endian(&result)
 }
 
 // returns (uint256 result)
@@ -20,89 +35,88 @@ pub fn mul_div(a: U256, b: U256, mut denominator: U256) -> Result<U256, UniswapV
         .overflowing_sub(prod_0)
         .0
         .overflowing_sub(U256::from((mm < prod_0) as u8))
-        .0; // Most significant 256 bits of the product
+        .0;
 
     // Handle non-overflow cases, 256 by 256 division
     if prod_1.is_zero() {
         if denominator == U256::zero() {
             return Err(UniswapV3MathError::DenominatorIsZero());
         }
-
-        Ok(prod_0 / denominator)
-    } else {
-        // Make sure the result is less than 2**256.
-        // Also prevents denominator == 0
-        if denominator <= prod_1 {
-            return Err(UniswapV3MathError::DenominatorIsLteProdOne());
-        }
-
-        ///////////////////////////////////////////////
-        // 512 by 256 division.
-        ///////////////////////////////////////////////
-        //
-
-        // Make division exact by subtracting the remainder from [prod1 prod0]
-        // Compute remainder using mulmod
-        let remainder = mul_mod(a, b, denominator);
-
-        // Subtract 256 bit number from 512 bit number
-        prod_1 = prod_1
-            .overflowing_sub(U256::from((remainder > prod_0) as u8))
-            .0;
-        prod_0 = prod_0.overflowing_sub(remainder).0;
-
-        // Factor powers of two out of denominator
-        // Compute largest power of two divisor of denominator.
-        // Always >= 1.
-        let mut twos = U256::zero()
-            .overflowing_sub(denominator)
-            .0
-            .bitand(denominator);
-
-        // Divide denominator by power of two
-        //TODO: this is in an assembly block, this should be able to underflow
-        denominator /= twos;
-
-        // Divide [prod1 prod0] by the factors of two
-        //TODO: this is in an assembly block, this should be able to underflow
-
-        prod_0 /= twos;
-
-        // Shift in bits from prod1 into prod0. For this we need
-        // to flip `twos` such that it is 2**256 / twos.
-        // If twos is zero, then it becomes one
-        //TODO: this is in an assembly block, this should be able to underflow
-        twos = (U256::zero().overflowing_sub(twos).0 / twos) + U256::one();
-
-        prod_0.bitor_assign(prod_1 * twos);
-
-        // Invert denominator mod 2**256
-        // Now that denominator is an odd number, it has an inverse
-        // modulo 2**256 such that denominator * inv = 1 mod 2**256.
-        // Compute the inverse by starting with a seed that is correct
-        // correct for four bits. That is, denominator * inv = 1 mod 2**4
-
-        let mut inv = (U256::from(3) * denominator) * (U256::from(3) * denominator);
-
-        // Now use Newton-Raphson iteration to improve the precision.
-        // Thanks to Hensel's lifting lemma, this also works in modular
-        // arithmetic, doubling the correct bits in each step.
-        inv *= U256::from(2) - denominator * inv;
-        inv *= U256::from(2) - denominator * inv; // inverse mod 2**8
-        inv *= U256::from(2) - denominator * inv; // inverse mod 2**16
-        inv *= U256::from(2) - denominator * inv; // inverse mod 2**32
-        inv *= U256::from(2) - denominator * inv; // inverse mod 2**64
-        inv *= U256::from(2) - denominator * inv; // inverse mod 2**128
-        inv *= U256::from(2) - denominator * inv; // inverse mod 2**256
-
-        // Because the division is now exact we can divide by multiplying
-        // with the modular inverse of denominator. This will give us the
-        // correct result modulo 2**256. Since the precoditions guarantee
-        // that the outcome is less than 2**256, this is the final result.
-        // We don't need to compute the high bits of the result and prod1
-        // is no longer required.
-        Ok(prod_0 * inv)
+        return Ok(prod_0 / denominator);
     }
+
+    // Make sure the result is less than 2**256.
+    // Also prevents denominator == 0
+    if denominator <= prod_1 {
+        return Err(UniswapV3MathError::DenominatorIsLteProdOne());
+    }
+
+    ///////////////////////////////////////////////
+    // 512 by 256 division.
+    ///////////////////////////////////////////////
+    //
+
+    // Make division exact by subtracting the remainder from [prod1 prod0]
+    // Compute remainder using mulmod
+    let remainder = mul_mod(a, b, denominator);
+
+    // Subtract 256 bit number from 512 bit number
+    prod_1 = prod_1
+        .overflowing_sub(U256::from((remainder > prod_0) as u8))
+        .0;
+    prod_0 = prod_0.overflowing_sub(remainder).0;
+
+    // Factor powers of two out of denominator
+    // Compute largest power of two divisor of denominator.
+    // Always >= 1.
+    let mut twos = U256::zero()
+        .overflowing_sub(denominator)
+        .0
+        .bitand(denominator);
+
+    // Divide denominator by power of two
+    //TODO: this is in an assembly block, this should be able to underflow
+    denominator /= twos;
+
+    // Divide [prod1 prod0] by the factors of two
+    //TODO: this is in an assembly block, this should be able to underflow
+
+    prod_0 /= twos;
+
+    // Shift in bits from prod1 into prod0. For this we need
+    // to flip `twos` such that it is 2**256 / twos.
+    // If twos is zero, then it becomes one
+    //TODO: this is in an assembly block, this should be able to underflow
+    twos = (U256::zero().overflowing_sub(twos).0 / twos) + U256::one();
+
+    prod_0.bitor_assign(prod_1 * twos);
+
+    // Invert denominator mod 2**256
+    // Now that denominator is an odd number, it has an inverse
+    // modulo 2**256 such that denominator * inv = 1 mod 2**256.
+    // Compute the inverse by starting with a seed that is correct
+    // correct for four bits. That is, denominator * inv = 1 mod 2**4
+
+    let mut inv = (U256::from(3) * denominator) * (U256::from(3) * denominator);
+
+    // Now use Newton-Raphson iteration to improve the precision.
+    // Thanks to Hensel's lifting lemma, this also works in modular
+    // arithmetic, doubling the correct bits in each step.
+    inv *= U256::from(2) - denominator * inv;
+    inv *= U256::from(2) - denominator * inv; // inverse mod 2**8
+    inv *= U256::from(2) - denominator * inv; // inverse mod 2**16
+    inv *= U256::from(2) - denominator * inv; // inverse mod 2**32
+    inv *= U256::from(2) - denominator * inv; // inverse mod 2**64
+    inv *= U256::from(2) - denominator * inv; // inverse mod 2**128
+    inv *= U256::from(2) - denominator * inv; // inverse mod 2**256
+
+    // Because the division is now exact we can divide by multiplying
+    // with the modular inverse of denominator. This will give us the
+    // correct result modulo 2**256. Since the precoditions guarantee
+    // that the outcome is less than 2**256, this is the final result.
+    // We don't need to compute the high bits of the result and prod1
+    // is no longer required.
+    Ok(prod_0 * inv)
 }
 
 pub fn mul_div_rounding_up(
@@ -121,4 +135,50 @@ pub fn mul_div_rounding_up(
     }
 
     Ok(result)
+}
+
+#[cfg(test)]
+mod test {
+    
+
+    use ethers::types::U256;
+
+    use crate::full_math::mul_mod;
+
+    use super::mul_div;
+
+    const Q128: U256 = U256([0, 0, 1, 0]);
+
+    #[test]
+    fn test_mul_mod() {
+        let result = mul_mod(U256::from(256), U256::from(5), U256::MAX);
+        assert_eq!(result, U256::from(1280));
+
+        let result = mul_mod(U256::from(100), U256::from(100), U256::from(21));
+        assert_eq!(result, U256::from(4));
+
+        let result = mul_mod(U256::from(100), U256::from(100), U256::from(0));
+        assert_eq!(result, U256::from(0));
+    }
+
+    #[test]
+    fn test_mul_div() {
+        //Revert if the denominator is zero
+        let result = mul_div(Q128, U256::from(5), U256::zero());
+        assert_eq!(result.err().unwrap().to_string(), "Denominator is 0");
+
+        // Revert if the denominator is zero and numerator overflows
+        let result = mul_div(Q128, Q128, U256::zero());
+        assert_eq!(
+            result.err().unwrap().to_string(),
+            "Denominator is less than or equal to prod_1"
+        );
+
+        // Revert if the output overflows uint256
+        let result = mul_div(Q128, Q128, U256::one());
+        assert_eq!(
+            result.err().unwrap().to_string(),
+            "Denominator is less than or equal to prod_1"
+        );
+    }
 }
