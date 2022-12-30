33ab78e2d2df73a58618f939cd50ff6002d78f7d
0xKitsune <0xKitsune@protonmail.com>
2022-12-30 11:56:19 -0500

    all tests passing for sqrt_price_math

--- src/sqrt_price_math.rs
+++ src/sqrt_price_math.rs
@@ -587,51 +587,6 @@ mod test {
             "require((product = amount * sqrtPX96) / amount == sqrtPX96 && numerator1 > product);"
         );
     }
-    #[test]
-    fn test_get_amount_1_delta() {
-        // returns 0 if liquidity is 0
-        let amount_1 = _get_amount_1_delta(
-            U256::from_dec_str("79228162514264337593543950336").unwrap(),
-            U256::from_dec_str("79228162514264337593543950336").unwrap(),
-            0,
-            true,
-        );
-
-        assert_eq!(amount_1.unwrap(), U256::zero());
-
-        // returns 0 if prices are equal
-        let amount_1 = _get_amount_1_delta(
-            U256::from_dec_str("79228162514264337593543950336").unwrap(),
-            U256::from_dec_str("87150978765690771352898345369").unwrap(),
-            0,
-            true,
-        );
-
-        assert_eq!(amount_1.unwrap(), U256::zero());
-
-        // returns 0.1 amount1 for price of 1 to 1.21
-        let amount_1 = _get_amount_1_delta(
-            U256::from_dec_str("79228162514264337593543950336").unwrap(),
-            U256::from_dec_str("87150978765690771352898345369").unwrap(),
-            1e18 as i128,
-            true,
-        )
-        .unwrap();
-
-        assert_eq!(
-            amount_1.clone(),
-            U256::from_dec_str("100000000000000000").unwrap()
-        );
-
-        let amount_1_rounded_down = _get_amount_1_delta(
-            U256::from_dec_str("79228162514264337593543950336").unwrap(),
-            U256::from_dec_str("87150978765690771352898345369").unwrap(),
-            1e18 as i128,
-            false,
-        );
-
-        assert_eq!(amount_1_rounded_down.unwrap(), amount_1.sub(1));
-    }
 
     #[test]
     fn test_get_amount_0_delta() {
@@ -699,10 +654,50 @@ mod test {
     }
 
     #[test]
-    fn test_get_next_sqrt_price_from_amount_1_rounding_down() {}
+    fn test_get_amount_1_delta() {
+        // returns 0 if liquidity is 0
+        let amount_1 = _get_amount_1_delta(
+            U256::from_dec_str("79228162514264337593543950336").unwrap(),
+            U256::from_dec_str("79228162514264337593543950336").unwrap(),
+            0,
+            true,
+        );
 
-    #[test]
-    fn test_get_next_sqrt_price_from_amount_0_rounding_up() {}
+        assert_eq!(amount_1.unwrap(), U256::zero());
+
+        // returns 0 if prices are equal
+        let amount_1 = _get_amount_1_delta(
+            U256::from_dec_str("79228162514264337593543950336").unwrap(),
+            U256::from_dec_str("87150978765690771352898345369").unwrap(),
+            0,
+            true,
+        );
+
+        assert_eq!(amount_1.unwrap(), U256::zero());
+
+        // returns 0.1 amount1 for price of 1 to 1.21
+        let amount_1 = _get_amount_1_delta(
+            U256::from_dec_str("79228162514264337593543950336").unwrap(),
+            U256::from_dec_str("87150978765690771352898345369").unwrap(),
+            1e18 as i128,
+            true,
+        )
+        .unwrap();
+
+        assert_eq!(
+            amount_1.clone(),
+            U256::from_dec_str("100000000000000000").unwrap()
+        );
+
+        let amount_1_rounded_down = _get_amount_1_delta(
+            U256::from_dec_str("79228162514264337593543950336").unwrap(),
+            U256::from_dec_str("87150978765690771352898345369").unwrap(),
+            1e18 as i128,
+            false,
+        );
+
+        assert_eq!(amount_1_rounded_down.unwrap(), amount_1.sub(1));
+    }
 
     #[test]
     fn test_swap_computation() {
