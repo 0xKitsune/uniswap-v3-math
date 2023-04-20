9c6d06b76542e691dc5012bec76d07ac7cb0a885
0xKitsune <0xKitsune@protonmail.com>
2023-04-20 17:48:41 -0400

    updated next_initialized_tick_within_one_word to take &HashMap<i16, U256>

--- src/tick_bitmap.rs
+++ src/tick_bitmap.rs
@@ -9,7 +9,7 @@ use std::{collections::HashMap, sync::Arc};
 //current_word is the current word in the TickBitmap of the pool based on `tick`. TickBitmap[word_pos] = current_word
 //Where word_pos is the 256 bit offset of the ticks word_pos.. word_pos := tick >> 8
 pub fn next_initialized_tick_within_one_word(
-    tick_bitmap: HashMap<i16, U256>,
+    tick_bitmap: &HashMap<i16, U256>,
     tick: i32,
     tick_spacing: i32,
     lte: bool,
